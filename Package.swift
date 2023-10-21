1868fc5b51633ad968761d259132f6d2013547ae
Pavlos Simas <simaspavlos@gmail.com>
2023-10-21 22:34:40 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "CocoaLumberjack",
     platforms: [
         .macOS(.v10_13),
-        .iOS(.v11),
+        .iOS(.v15),
         .tvOS(.v11),
         .watchOS(.v4),
     ],
